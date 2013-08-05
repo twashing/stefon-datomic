c536ee890c920c4d06559321c89ee198abfad1b0
Timothy Washington <twashing@gmail.com>
2013-08-05 13:52:04 -0700

    -- adding smallish description to the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject stefon-datomic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Datomic database adapter for the Stefon blogging engine."
+  :url "https://github.com/twashing/stefon-datomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
