278ca607f696e0c8b2eeee53b32197d6e0c4974f
Timothy Washington <twashing@gmail.com>
2013-08-12 18:30:10 -0700

    -- first working test to seend a message to the kernel, and get back a list of domain objects

--- project.clj
+++ project.clj
@@ -4,10 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic "0.8.3335"
+                  :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
+
                  #_[stefon "0.1.0-SNAPSHOT"]]
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                   :dependencies [[lamina "0.5.0-rc3"]
+                                  [cljs-uuid "0.0.4"]
+                                  [org.clojure/tools.namespace "0.2.3"]
                                   [speclj "2.5.0"]]}}
 
   :plugins [[speclj "2.5.0"]]
--- spec/stefon_datomic/plugin_spec.clj
+++ spec/stefon_datomic/plugin_spec.clj
@@ -1,22 +1,24 @@
 (ns stefon-datomic.plugin-spec
 
   (:require [speclj.core :refer :all]
-            [stefon.shell :as shell]))
+            [stefon.shell :as shell]
+            [stefon.shell.plugin :as plugin]))
 
 
 (describe "plugin should be able to attach to a running Stefon instance"
 
-
-          (before (shell/start-system))
-          (after (shell/stop-system))
-
           (it "Should attach to a running Stefon instance"
 
-              (let [handler-fn (fn [message])
-                    sender-fn (shell/attach-plugin handler-fn)
+              (let [sys1 (shell/create-system)
+                    sys2 (shell/start-system sys1)
+
+                    handler-fn (fn [message] (println "Zzz ..."))
+                    sender-fn (plugin/attach-plugin @sys2 handler-fn)
 
                     result-promise (sender-fn {:stefon.domain {:parameters nil}})]
 
+                (println ">> 1" result-promise)
                 (should-not-be-nil @result-promise)
+                (println ">> 2")
 
                 (should= 1 1))))
