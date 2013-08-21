92abe0b8966e3455ec4ea73267229c4c333caeb2
Timothy Washington <twashing@gmail.com>
2013-08-21 08:45:25 -0700

    -- adding test to return a list of domain schema

--- spec/stefon_datomic/plugin_spec.clj
+++ spec/stefon_datomic/plugin_spec.clj
@@ -12,7 +12,7 @@
               (let [sys1 (shell/create-system)
                     sys2 (shell/start-system sys1)
 
-                    handler-fn (fn [message] (println "Zzz ..."))
+                    handler-fn (fn [message] (println "Handler message, after :stefon.domain > " message))
                     sender-fn (plugin/attach-plugin @sys2 handler-fn)
 
                     result-promise (sender-fn {:stefon.domain {:parameters nil}})]
@@ -22,7 +22,25 @@
 
                 (should= 1 1)))
 
+          (it "Should return a list of domain schema"
+
+              (let [sys1 (shell/create-system)
+                    sys2 (shell/start-system sys1)
+
+                    handler-fn (fn [message] (println "Handler message, after :stefon.domain.schema > " message))
+                    sender-fn (plugin/attach-plugin @sys2 handler-fn)
+
+                    result-promise (sender-fn {:stefon.domain.schema {:parameters nil}})]
+
+                (println ">> " @result-promise)
+                (should-not-be-nil @result-promise)
+                (should= '(:posts :assets :tags) (keys @result-promise))
+                (should= {:posts [{:name :id, :cardinality :one, :type :uuid} {:name :title, :cardinality :one, :type :string} {:name :content, :cardinality :one, :type :string} {:name :content-type, :cardinality :one, :type :string} {:name :created-date, :cardinality :one, :type :date} {:name :modified-date, :cardinality :one, :type :date}], :assets [{:name :id, :cardinality :one, :type :uuid} {:name :name, :cardinality :one, :type :string} {:name :type, :cardinality :one, :type :string} {:name :asset, :cardinality :one, :type :string}], :tags [{:name :id, :cardinality :one, :type :uuid} {:name :name, :cardinality :one, :type :string}]}
+                         @result-promise)))
+
+
           ;; check that kernel / shell is running
+
 
           ;; attach itself to kernel
 
