eaf5481bae9844ea0d398f6115c02c0cb3adda81
Timothy Washington <twashing@gmail.com>
2013-09-13 09:51:09 -0700

    -- adding back list function test for tag

--- spec/stefon_datomic/crud_tags_spec.clj
+++ spec/stefon_datomic/crud_tags_spec.clj
@@ -146,29 +146,8 @@
 
                   (should (empty? result-after)) )))
 
-          #_(it "Should find by attributes: content-type & created-date"
 
-              ;; create 4, 2 txt, and 2 md files; make one of them have a different created-date
-              ;;   then find the md files... from the DB
-              ;;   then find the one with a different created-date... from the DB
-              (let [;; create DB & get the connection
-                    result (pluginD/bootstrap-stefon)
-
-                    ;; add datom
-                    date-one (-> (java.text.SimpleDateFormat. "MM/DD/yyyy") (.parse "09/01/2013"))
-                    one (crud/create (:conn result) :tag {:title "t" :content "c" :content-type "c/t" :created-date date-one :modified-date date-one})
-                    two (crud/create (:conn result) :tag {:title "two" :content "two content" :content-type "c/t" :created-date date-one :modified-date date-one})
-                    three (crud/create (:conn result) :tag {:title "three" :content "three content" :content-type "c/t" :created-date date-one :modified-date date-one})
-
-                    qresult (crud/retrieve (:conn result) {:content-type "c/t" :title "t"})
-                    qresult-many (crud/retrieve (:conn result) {:content-type "c/t"})]
-
-                (should (seq? qresult))
-                (should-not (empty? qresult))
-                (should= 1 (count qresult))
-                (should= 3 (count qresult-many))))
-
-          #_(it "Should list created tags"
+          (it "Should list created tags"
 
               ;; create 3, then list them out... from the DB
               (let [conn (populate-with-tags)
