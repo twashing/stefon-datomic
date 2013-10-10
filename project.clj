c8d9f03bb4adff7c50d43bf5f4b99f0f52a1e607
Timothy Washington <twashing@gmail.com>
2013-10-10 09:18:28 -0700

    -- removing uneeded stefon dependency declaration

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.8.4143"
                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
-
-                 #_[stefon "0.1.0-SNAPSHOT"]]
+                 ]
 
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
