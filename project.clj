52fe80161e070c886c1dc1b54caa2490da443aa2
Timothy Washington <twashing@gmail.com>
2013-10-11 00:14:24 -0700

    -- trying to remove the exclusions

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.8.4143"
+                 #_[com.datomic/datomic-free "0.8.4143"
                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
+                 [com.datomic/datomic-free "0.8.4143"]
                  #_[org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
                  [prismatic/schema "0.1.5"]
